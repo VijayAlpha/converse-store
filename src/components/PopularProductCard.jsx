9b1d72066d43dbee981a3128dbaa547966fcb4ba
Vijay Murugan <vijaymurugan177@gmail.com>
2024-02-17 23:42:16 +0530

    All Set

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { star } from "../assets/icons";
 import { products } from "../assets/constants";
 import { shoe10, shoe11, shoe12, shoe7, shoe9 } from "../assets/images";
 
@@ -24,17 +23,17 @@ const PopularProductCard = ({ imgURL, name, price, isLast }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={displayedImage} alt={name} className="w-[280px] h-[280px]" />
-      <div className="mt-8 flex justify-start gap-2.5">
+      {/* <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">
           (4.5)
         </p>
-      </div>
+      </div> */}
       <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
         {name}
       </h3>
       <p className="mt-2 font-semibold font-montserrat text-gray-900 text-2xl leading-normal">
-        {price}
+        $Free
       </p>
     </div>
   );
